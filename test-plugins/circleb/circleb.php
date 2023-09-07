55c4202f4bb1b2c399464d9584c3bae2834648d5
Andy Fragen <andy@thefragens.com>
2023-09-07 12:47:15 -0700

    add all testing plugins

---
+++ test-plugins/circleb/circleb.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Circular Dependency Test
+ *
+ * @package wp-plugin-dependencies
+ *
+ * Plugin Name: Test Circular Dependency B
+ * Author: Andy Fragen
+ * Version: 0.1
+ * Description: Circular dependency example.
+ * Requires Plugins:  circlea
+ */
---
+++ test-plugins/test-dependencies1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Testing plugin
+ *
+ * @package wp-plugin-dependencies
+ *
+ * Plugin Name: Test Plugin Dependencies 1
+ * Author: WordPress Core Contributors
+ * Version: 0.1
+ * Description: Testing inclusion of dot org plugins in the Dependencies tab.
+ * Requires Plugins: hello-dolly, -my-plugin, my-plugin-, my--plugin
+ */
