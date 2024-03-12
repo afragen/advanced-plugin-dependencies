64efa9d4b42072403155de2dbe1b367997b2f67f
Andy Fragen <andy@thefragens.com>
2024-03-11 17:18:34 -0700

    bump

--- plugin.php
+++ plugin.php
@@ -13,7 +13,7 @@
  * Plugin URI:  https://wordpress.org/plugins/advanced-plugin-dependencies
  * Description:  Add plugin install dependencies tab, support for non dot org plugin cards, and information about dependencies.
  * Author: Andy Fragen, Colin Stewart, Paul Biron
- * Version: 0.8.3.8
+ * Version: 0.8.4
  * License: MIT
  * Network: true
  * Requires at least: 6.5
