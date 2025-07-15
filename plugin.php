d1d62895573a9ced0bb87cc2aa198925f35cb439
Andy Fragen <andy@thefragens.com>
2025-07-15 11:33:43 -0700

    just a bit more

--- plugin.php
+++ plugin.php
@@ -13,7 +13,7 @@
  * Plugin URI:  https://wordpress.org/plugins/advanced-plugin-dependencies
  * Description:  Add plugin install dependencies tab, support for non dot org plugin cards, and information about dependencies.
  * Author: Andy Fragen, Colin Stewart, Paul Biron
- * Version: 0.10.0
+ * Version: 0.11.0
  * License: MIT
  * Text Domain: advanced-plugin-dependencies
  * Network: true
@@ -25,17 +25,20 @@
 
 namespace Advanced_Plugin_Dependencies;
 
+const PLUGIN_DIR = __DIR__;
+
+bootstrap();
+
 /**
  * Initialize, load filters, and get started.
  *
  * @return void
  */
 function bootstrap() {
-	require_once __DIR__ . '/src/advanced-plugin-dependencies.php';
-
 	add_filter( 'install_plugins_tabs', __NAMESPACE__ . '\\add_install_tab', 10, 1 );
 	add_filter( 'install_plugins_table_api_args_dependencies', __NAMESPACE__ . '\\add_install_dependency_args', 10, 1 );
 
+	add_action( 'init', __NAMESPACE__ . '\\init' );
 	add_action( 'install_plugins_dependencies', 'display_plugins_table' );
 	add_action(
 		'install_plugins_table_header',
@@ -48,7 +51,15 @@ function bootstrap() {
 		}
 	);
 }
-bootstrap();
+
+/**
+ * Initialize.
+ *
+ * @return void
+ */
+function init(): void {
+	require_once PLUGIN_DIR . '/src/advanced-plugin-dependencies.php';
+}
 
 /**
  * Add 'Dependencies' tab to 'Plugin > Add New'.
