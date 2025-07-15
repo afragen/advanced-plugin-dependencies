1facdabcba088388d4a6cc59044aa08b0e0321e3
Andy Fragen <andy@thefragens.com>
2025-07-15 09:36:29 -0700

    remove Init class for bootstrap

--- CHANGES.md
+++ CHANGES.md
@@ -3,3 +3,4 @@
 * fork from WordPress/wp-plugin-dependencies to add back Dependencies tab, and support non-dot org dependencies
 * add caching to `Advanced_Plugin_Dependencies::fetch_non_dotorg_dependency_data()`
 * sanitize download links
+* remove `Init` class
--- plugin.php
+++ plugin.php
@@ -13,7 +13,7 @@
  * Plugin URI:  https://wordpress.org/plugins/advanced-plugin-dependencies
  * Description:  Add plugin install dependencies tab, support for non dot org plugin cards, and information about dependencies.
  * Author: Andy Fragen, Colin Stewart, Paul Biron
- * Version: 0.9.6
+ * Version: 0.10.0
  * License: MIT
  * Text Domain: advanced-plugin-dependencies
  * Network: true
@@ -25,76 +25,56 @@
 
 namespace Advanced_Plugin_Dependencies;
 
-/*
- * Exit if called directly.
- * PHP version check and exit.
+/**
+ * Initialize, load filters, and get started.
+ *
+ * @return void
  */
-if ( ! defined( 'WPINC' ) ) {
-	die;
+function bootstrap() {
+	require_once __DIR__ . '/src/advanced-plugin-dependencies.php';
+
+	add_filter( 'install_plugins_tabs', __NAMESPACE__ . '\\add_install_tab', 10, 1 );
+	add_filter( 'install_plugins_table_api_args_dependencies', __NAMESPACE__ . '\\add_install_dependency_args', 10, 1 );
+
+	add_action( 'install_plugins_dependencies', 'display_plugins_table' );
+	add_action(
+		'install_plugins_table_header',
+		static function () {
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			$tab = isset( $_GET['tab'] ) ? sanitize_title_with_dashes( wp_unslash( $_GET['tab'] ) ) : '';
+			if ( 'dependencies' === $tab ) {
+				echo '<p>' . esc_html__( 'These suggestions are based on dependencies required by installed plugins.', 'advanced-plugin-dependencies' ) . '</p>';
+			}
+		}
+	);
+}
+bootstrap();
+
+/**
+ * Add 'Dependencies' tab to 'Plugin > Add New'.
+ *
+ * @param array $tabs Array of plugin install tabs.
+ * @return array
+ */
+function add_install_tab( $tabs ) {
+	$tabs['dependencies'] = esc_html_x( 'Dependencies', 'Plugin Installer', 'advanced-plugin-dependencies' );
+
+	return $tabs;
 }
 
 /**
- * Class Init
+ * Add args to plugins_api().
+ *
+ * @param array $args Array of arguments to plugins_api().
+ * @return array
  */
-class Init {
+function add_install_dependency_args( $args ) {
+	$args = array(
+		'page'     => 1,
+		'per_page' => 36,
+		'locale'   => get_user_locale(),
+		'browse'   => 'dependencies',
+	);
 
-	/**
-	 * Initialize, load filters, and get started.
-	 *
-	 * @return void
-	 */
-	public function __construct() {
-		require_once __DIR__ . '/src/advanced-plugin-dependencies.php';
-
-		add_filter( 'install_plugins_tabs', array( $this, 'add_install_tab' ), 10, 1 );
-		add_filter( 'install_plugins_table_api_args_dependencies', array( $this, 'add_install_dependency_args' ), 10, 1 );
-
-		add_action( 'install_plugins_dependencies', 'display_plugins_table' );
-		add_action(
-			'install_plugins_table_header',
-			static function () {
-				// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-				$tab = isset( $_GET['tab'] ) ? sanitize_title_with_dashes( wp_unslash( $_GET['tab'] ) ) : '';
-				if ( 'dependencies' === $tab ) {
-					echo '<p>' . esc_html__( 'These suggestions are based on dependencies required by installed plugins.', 'advanced-plugin-dependencies' ) . '</p>';
-				}
-			}
-		);
-	}
-
-	/**
-	 * Add 'Dependencies' tab to 'Plugin > Add New'.
-	 *
-	 * @param array $tabs Array of plugin install tabs.
-	 * @return array
-	 */
-	public function add_install_tab( $tabs ) {
-		$tabs['dependencies'] = esc_html_x( 'Dependencies', 'Plugin Installer', 'advanced-plugin-dependencies' );
-
-		return $tabs;
-	}
-
-	/**
-	 * Add args to plugins_api().
-	 *
-	 * @param array $args Array of arguments to plugins_api().
-	 * @return array
-	 */
-	public function add_install_dependency_args( $args ) {
-		$args = array(
-			'page'     => 1,
-			'per_page' => 36,
-			'locale'   => get_user_locale(),
-			'browse'   => 'dependencies',
-		);
-
-		return $args;
-	}
+	return $args;
 }
-
-add_action(
-	'init',
-	function () {
-		new Init();
-	}
-);
